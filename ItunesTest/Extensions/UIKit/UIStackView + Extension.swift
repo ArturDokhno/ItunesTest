1f097deb7290bbad8b5ce5ce0751eb2c16223122
ArturDokhno <artur.dohno@icloud.com>
2022-05-24 21:36:20 +0500

    Создание экрана авторизации

---
+++ ItunesTest/Extensions/UIKit/UIStackView + Extension.swift
@@ -0,0 +1,18 @@
+//
+//  UIStackView + Extension.swift
+//  ItunesTest
+//
+//  Created by Артур Дохно on 24.05.2022.
+//
+
+import UIKit
+
+extension UIStackView {
+    convenience init(arrangedSubviews: [UIView], axis: NSLayoutConstraint.Axis, spacing: CGFloat, distribution: UIStackView.Distribution) {
+        self.init(arrangedSubviews: arrangedSubviews)
+        self.axis = axis
+        self.spacing = spacing
+        self.distribution = distribution
+        self.translatesAutoresizingMaskIntoConstraints = false
+    }
+}
